1f8b0d94840ccb7b529404ff5d700afd984199b8
James Lacson <chickenchowmien@gmail.com>
2018-06-10 17:23:03 -0400

    commit

--- src/test/java/api/tests/ReqresApi.java
+++ src/test/java/api/tests/ReqresApi.java
@@ -3,6 +3,7 @@ package api.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.google.gson.Gson;
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
@@ -14,13 +15,13 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
 
 public class ReqresApi {
-	
+
 	/*
 	 * Send a get request to https://reqres.in/api/users
 	 * Including query param -> page=2
 	 * Accept type is json
 	 * Verify status code 200, verify response body
-	 * 
+	 *
 	 */
 	@Test
 	public void getUsersTest() {
@@ -28,32 +29,37 @@ public class ReqresApi {
 //		.and().params("page", 2)
 //		.when().get("https://reqres.in/api/users")
 //		.then().assertThat().statusCode(200);
-		
+
 		Response response = given().accept(ContentType.JSON)
 							.and().params("page", 2)
 							.when().get("https://reqres.in/api/users");
-		
+
 		System.out.println(response.getStatusLine());
 		System.out.println(response.getContentType());
 		System.out.println(response.headers());
 		System.out.println(response.body().asString());
-		
+
 	//add assertions for parts of response.
 		assertEquals(200,response.statusCode());
 		assertTrue(response.contentType().contains("application/json"));
-		
+
 		Header header = new Header("X-Powered-By", "Express");
 		assertTrue(response.headers().asList().contains(header));
-		
+
 		JsonPath json = response.jsonPath();
-		
+
 		assertEquals(12, json.getInt("total"));
 		assertEquals(4, json.getInt("total_pages"));
-		
+
 		assertEquals(4,json.getInt("data.id[0]"));
 		//Verify that Charles's id is 5
 		assertEquals(5,json.getInt("data.find{it.first_name == 'Charles'}.id"));
-		
+		assertEquals("Tracey", json.getString("data.find{it.id == 6}.first_name"));
+		assertEquals("Ramos", json.getString("data.find{it.id == 6}.last_name"));
+
+		Gson gson = new Gson();
+		System.out.println(gson.fromJson(gson.toJson(new Person("James") ),Person.class));
+
 //		{
 //		    "page": 2,
 //		    "per_page": 3,
@@ -80,13 +86,23 @@ public class ReqresApi {
 //		        }
 //		    ]
 //		}
-		
+
 	}
-	
+
 }
 
 
 
+class Person{
+	public Person(String name) {
+		this.name=name;
+	}
+	public String name;
 
+	@Override
+	public String toString() {
+		return name;
+	}
+}
 
 
