fb2bc1f2b6eb7dbb90143ca28d813f0dc9c03b73
Tstepany8 <33642720+Tstepany8@users.noreply.github.com>
2018-06-24 18:26:31 -0400

    Adding new Section:Deployment

--- src/test/java/Java/EncapPr.java
+++ src/test/java/Java/EncapPr.java
@@ -1,6 +1,7 @@
 package Java;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class EncapPr {
@@ -11,12 +12,39 @@ public class EncapPr {
 List <locators> lc= new ArrayList<>();
 lc.add(new locators("//td[preceding-sibling::td='t']", "E[A*='t']"));
 lc.add(new locators("E[contains(@A,'t')" ,"E[A*='t']"));
+lc.add(new locators("//div[. = ' ']" ,"#id"));
+lc.add(new locators("remove" ,"#id"));
+lc.add(new locators("//div[. = ' ']" ,"#id"));
+lc.add(new locators("remove" ,"remove"));
+lc.add(new locators("//div[. = ' ']" ,"#id"));
 
+
+Iterator it =lc.iterator();
+
+while (it.hasNext()) {
+	System.out.println(it.next());
+}
     }
 
 }
+/*System.out.println("Size Before "+ lc.size());
+for(int i=0; i<lc.size();i++) {
+	if (lc.get(i).getXpath().equals("remove")) {
+	lc.remove(i);
+		
+	}
+}
+System.out.println("Size After "+ lc.size());
+}
+
+}*/
 
  class locators{
+
+@Override
+	public String toString() {
+		return "locators [xpath=" + xpath + ", css=" + css + "]";
+	}
 
 public locators(String xpath, String css){
     this.xpath=xpath;
@@ -47,3 +75,4 @@ public void setCss(String css){
 }
 
 }
+
