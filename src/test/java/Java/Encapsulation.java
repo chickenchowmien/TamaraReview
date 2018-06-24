a377802d0ca35e6d3dbf06eb6aa9e81e87c2321f
Tstepany8 <33642720+Tstepany8@users.noreply.github.com>
2018-06-24 14:32:32 -0400

    Encapsulation

---
+++ src/test/java/Java/Encapsulation.java
@@ -0,0 +1,53 @@
+package Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Encapsulation {
+	
+
+    public static void main (String []args){
+
+List <locators> lc= new ArrayList<>();
+lc.add(new locators("//td[preceding-sibling::td='t']" , " E[A*='t' "));
+lc.add(new locators("E[contains(@A,'t')" , "E[A*='t']"));
+
+
+
+
+
+    }
+
+}
+
+ class locators{
+
+public locators(String xpath, String css){
+    this.xpath=xpath;
+    this.css=css;
+
+}
+
+private String xpath;
+private String css;
+
+public String getXpath(){
+return xpath;
+
+}
+
+public void setXpath(String xpath){
+this.xpath= xpath;
+}
+
+public String getCss(){
+    return css;
+
+}
+
+public void setCss(String css){
+    this.css=css;
+
+}
+
+}
